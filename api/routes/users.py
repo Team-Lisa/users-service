cb494c9f95eb5639cc5221a1454ef6fdbaa55fcf
vikyzambi <viky_zambi95@hotmail.com>
2021-10-17 21:15:37 -0300

    changed update token entopoint  to recive a Token model

--- api/routes/users.py
+++ api/routes/users.py
@@ -4,6 +4,7 @@ from api.models.requests.user import User
 from api.controllers.user_controller import UserController
 from api.models.responses.user import User as UserResponse
 from api.models.responses.users import Users as UsersResponse
+from api.models.requests.token import Token
 
 
 router = APIRouter(tags=["Users"])
@@ -19,8 +20,8 @@ async def find_user(email: str = ""):
     return UserController.find_by_email(email)
 
 @router.patch("/users", response_model=UserResponse)
-async def update_expo_token(user:User):
-    return UserController.update_expo_token(user.expo_token,user.email)
+async def update_expo_token(token: Token, email: str = ""):
+    return UserController.update_expo_token(token.expo_token, email)
 
 @router.delete("/users")
 async def delete_users(email: str = ""):
